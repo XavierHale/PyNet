a7dc0f02df014e4d01a54af8e531039935f1ea88
XavierHale <81330980+XavierHale@users.noreply.github.com>
2022-08-27 19:11:00 -0400

    Restarted the developent of the login system

--- server.py
+++ server.py
@@ -1,9 +1,25 @@
-cushos = input("custom host? y/n")
-if (cushos == y):
-    host = input("enter custom host:")
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-else:
-    serversocket.bind((socket.gethostname(), 80))
-    
+import socket
+# binding ports and crap
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind((socket.gethostname(), 80))
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+hostname = socket.gethostname()  
+SERVER_HOST = socket.gethostbyname(hostname)
+print("Listening as", SERVER_HOST)
 serversocket.listen(5)
+conn, addr = serversocket.accept()
+with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+            conn.sendall(data)
